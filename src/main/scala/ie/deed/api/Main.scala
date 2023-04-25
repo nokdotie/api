bdd9333bf31ed784fccbc25055c28fe86155f7d6
Philippe Vinchon <p.vinchon@gmail.com>
2023-04-25 15:49:16 +0100

    Add swagger file for all apis

--- src/main/scala/ie/deed/api/Main.scala
+++ src/main/scala/ie/deed/api/Main.scala
@@ -4,11 +4,15 @@ import zio._
 import zio.http._
 import zio.http.ServerConfig.default.address.getPort
 import ie.deed.api.proxies.BuildingEnergyRatingProxy
+import ie.deed.api.apps.SwaggerHttp
 import scala.util.chaining.scalaUtilChainingOps
 
 object Main extends ZIOAppDefault {
 
-  private val app: App[Client] = BuildingEnergyRatingProxy.http.pipe {
+  private val app: App[Client] = (
+    BuildingEnergyRatingProxy.http ++
+      SwaggerHttp.http
+  ).pipe {
     _
       @@ HttpAppMiddleware.debug
       @@ HttpAppMiddleware.cors()
