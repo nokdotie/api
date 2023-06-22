c700fd7120fee9f7fd5e85d1b1e95f0460c25d84
Philippe Vinchon <p.vinchon@gmail.com>
2023-06-22 14:45:54 +0100

    Remove transition code

--- src/main/scala/ie/nok/api/graphql/adverts/Advert.scala
+++ src/main/scala/ie/nok/api/graphql/adverts/Advert.scala
@@ -4,12 +4,10 @@ import java.time.Instant
 
 case class Advert(
     advertUrl: String,
-    advertPrice: Int,
     advertPriceInEur: Int,
     propertyAddress: String,
     propertyImageUrls: List[String],
     propertySizeInSqtMtr: BigDecimal,
-    propertySizeinSqtMtr: BigDecimal,
     propertyBedroomsCount: Int,
     propertyBathroomsCount: Int
 )
@@ -18,11 +16,9 @@ object Advert {
   def fromInternal(internal: ie.nok.adverts.Advert): Advert =
     Advert(
       advertUrl = internal.advertUrl,
-      advertPrice = internal.advertPriceInEur,
       advertPriceInEur = internal.advertPriceInEur,
       propertyAddress = internal.propertyAddress,
       propertyImageUrls = internal.propertyImageUrls,
-      propertySizeinSqtMtr = internal.propertySizeInSqtMtr,
       propertySizeInSqtMtr = internal.propertySizeInSqtMtr,
       propertyBedroomsCount = internal.propertyBedroomsCount,
       propertyBathroomsCount = internal.propertyBathroomsCount
