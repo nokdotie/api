b96886a665715346f67933e0b92b6a91c8c689ef
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2024-03-12 16:41:36 +0000

    Adding PropertyType filter (#46)

--- src/main/scala/ie/nok/api/utils/filters/StringFilter.scala
+++ src/main/scala/ie/nok/api/utils/filters/StringFilter.scala
@@ -1,10 +1,13 @@
 package ie.nok.api.utils.filters
 
-import caliban.schema.{Schema, ArgBuilder}
+import caliban.schema.{ArgBuilder, Schema}
 import ie.nok.stores.filters.StringFilter as Filter
 
+import scala.util.chaining.scalaUtilChainingOps
+
 case class StringFilter(
-    contains: Option[String]
+    contains: Option[String],
+    equals: Option[String]
 )
 
 object StringFilter {
@@ -12,10 +15,13 @@ object StringFilter {
   given Schema[Any, StringFilter] = Schema.gen
 
   def toStoreFilter(filter: StringFilter): Filter =
-    filter.contains
-      .map(_.trim)
-      .fold(Filter.Empty)(
-        Filter.ContainsCaseInsensitive(_)
-      )
-
+    List(
+      filter.contains.map(Filter.ContainsCaseInsensitive(_)),
+      filter.equals.map(Filter.EqualsCaseInsensitive(_))
+    ).flatten
+      .pipe {
+        case Nil          => Filter.Empty
+        case head :: Nil  => head
+        case head :: tail => Filter.And(head, tail: _*)
+      }
 }
