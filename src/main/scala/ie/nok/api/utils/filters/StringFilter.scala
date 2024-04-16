66a2a794fffea7d00658510ed97e0637c6355629
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2024-04-16 10:51:06 +0100

    Adding StringFilter.in for PropertyType filter multiselect (#53)

--- src/main/scala/ie/nok/api/utils/filters/StringFilter.scala
+++ src/main/scala/ie/nok/api/utils/filters/StringFilter.scala
@@ -7,21 +7,27 @@ import scala.util.chaining.scalaUtilChainingOps
 
 case class StringFilter(
     contains: Option[String],
-    equals: Option[String]
+    equals: Option[String],
+    in: Option[List[String]]
 )
 
 object StringFilter {
   given ArgBuilder[StringFilter]  = ArgBuilder.gen
   given Schema[Any, StringFilter] = Schema.gen
 
-  def toStoreFilter(filter: StringFilter): Filter =
-    List(
-      filter.contains.map(Filter.ContainsCaseInsensitive(_)),
-      filter.equals.map(Filter.EqualsCaseInsensitive(_))
-    ).flatten
+  private def toFilter(list: List[Filter], f: (Filter, Seq[Filter]) => Filter): Filter =
+    list
       .pipe {
         case Nil          => Filter.Empty
         case head :: Nil  => head
-        case head :: tail => Filter.And(head, tail: _*)
+        case head :: tail => f(head, tail)
       }
+
+  def toStoreFilter(filter: StringFilter): Filter =
+    List(
+      filter.contains.map(Filter.ContainsCaseInsensitive(_)),
+      filter.equals.map(Filter.EqualsCaseInsensitive(_)),
+      filter.in.map(_.map(Filter.EqualsCaseInsensitive(_))).map(toFilter(_, Filter.Or.apply))
+    ).flatten
+      .pipe { toFilter(_, Filter.And.apply) }
 }
